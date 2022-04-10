3a6f954a6197ee6aa8511649629febea206dfa68
Henrique Andrade <henrique.ccda@gmail.com>
2022-04-10 16:34:27 -0300

    Fim do curso.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Formação React da Alura
 
-1. [Curso de Fundamentos de React: escrevendo com Typescript](https://github.com/HenriqueCCdA/FormacaoReactAlura) Inicio dia 05/04/2022
+1. [Curso de Fundamentos de React: escrevendo com Typescript](https://github.com/HenriqueCCdA/FormacaoReactAlura) Inicio dia 05/04/2022 - Fim dia 10/04/2022
--- fundamentos_de_react/alura-studies/src/components/Cronometro/index.tsx
+++ fundamentos_de_react/alura-studies/src/components/Cronometro/index.tsx
@@ -7,10 +7,11 @@ import { tempoParaSegundos } from "../../common/utils/date";
 
 interface Props {
     selecionado: ITarefa | undefined
+    finalizarTarefa: () => void;
 }
 
 
-export default function Cronometro({ selecionado }: Props) {
+export default function Cronometro({ selecionado, finalizarTarefa }: Props) {
     const [tempo, setTempo] = useState<number>();
 
     useEffect( () => {
@@ -19,6 +20,15 @@ export default function Cronometro({ selecionado }: Props) {
         }
     }, [selecionado])
 
+    function regressiva(contador: number = 0) {
+        setTimeout( () => {
+            if(contador > 0) {
+                setTempo(contador - 1);
+                return regressiva(contador - 1);
+            }
+            finalizarTarefa();
+        }, 1000)
+    }
 
     return (
         <div className={style.cronometro}>
@@ -27,7 +37,7 @@ export default function Cronometro({ selecionado }: Props) {
             <div className={style.relogioWrapper}>
                 <Relogio tempo={tempo} />
             </div>
-            <Botao>
+            <Botao onClick={() => regressiva(tempo)}>
                 Começar!
             </Botao>
         </div>

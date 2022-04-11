d45a0294509273bcd9aaa68d69024631398d887e
Henrique Andrade <henrique.ccda@gmail.com>
2022-04-11 16:36:04 -0300

    Aula 1.

---
+++ arquivo_estaticos_02/aluroni/src/pages/Cardapio/index.tsx
@@ -0,0 +1,12 @@
+import styles from './Cardapio.modulo.scss'
+
+
+export default function Cardapio() {
+    return (
+        <main>
+            <nav className="menu">
+
+            </nav>
+        </main>
+    )
+}

973ff999cecb71d9d6be981cc16adcdb4ddb47d3
Henrique Andrade <henrique.ccda@gmail.com>
2022-04-26 14:08:58 -0300

    Fim da aula3.

--- biblioteca_react_router_03/aluroni/src/routes.js
+++ biblioteca_react_router_03/aluroni/src/routes.js
@@ -1,3 +1,5 @@
+import Menu from 'components/Menu';
+import PaginaPadrao from 'components/PaginaPadrao';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -5,11 +7,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function AppRouter() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Inicio />} />
-        <Route path='/cardapio' element={<Cardapio />} />
-      </Routes>
-    </Router>
+    <main>
+      <Router>
+        <Menu />
+        <Routes>
+          <Route path='/' element={<PaginaPadrao/>}>
+            <Route index element={<Inicio />} />
+            <Route path='cardapio' element={<Cardapio />} />
+          </Route>
+        </Routes>
+      </Router>
+    </main>
   );
 }

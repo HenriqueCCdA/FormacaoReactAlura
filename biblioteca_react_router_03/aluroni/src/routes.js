91f80557be0ec4e2483e0a2e2db232fd70d731f2
Henrique Andrade <henrique.ccda@gmail.com>
2022-05-02 17:05:01 -0300

    Aula 5.

--- biblioteca_react_router_03/aluroni/src/routes.js
+++ biblioteca_react_router_03/aluroni/src/routes.js
@@ -4,13 +4,14 @@ import PaginaPadrao from 'components/PaginaPadrao';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
 import NotFound from 'pages/NotFound';
+import Prato from 'pages/Prato';
 import Sobre from 'pages/Sobre';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 export default function AppRouter() {
   return (
-    <main>
+    <main className='container'>
       <Router>
         <Menu />
         <Routes>
@@ -19,6 +20,7 @@ export default function AppRouter() {
             <Route path='cardapio' element={<Cardapio />} />
             <Route path='sobre' element={<Sobre />} />
           </Route>
+          <Route path='prato/:id' element={<Prato />} />
           <Route path='*' element={<NotFound /> } />
         </Routes>
         <Footer />
